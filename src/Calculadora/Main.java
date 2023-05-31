6f1099a610b7b46a90e56c06c5c20ea59d4c4e87
Simon <simonrivas41@gmail.com>
2023-05-31 14:32:09 +0200

    segundo commit

--- src/Calculadora/Main.java
+++ src/Calculadora/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Se añade otra linea");
+		System.out.println("Otro cambio mas para ver la diferencia");
 
 	}
 
